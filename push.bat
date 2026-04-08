596f844cb35e9620f52a16418896d6cfab59e60b
tim3lrom <timelrom@gmail.com>
2026-04-08 17:55:19 -0400

    Created a staging_v2.py script that added clean comments, implied_coupon_rate to Snowflake

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "Created ICR calculation using Snowflake fundamentals, maps to Damodaran Synthetic Rating for default spread, pulls live 10Y Treasury from FMP, calculates pre and post-tax Cost of Debt (Kd)"
+git commit -m "Created a staging_v2.py script that added clean comments, implied_coupon_rate to Snowflake"
 git push origin main
